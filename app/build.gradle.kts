edf2e7ec6dba5f16ba30a5ec699327a78444951c
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2022-06-04 12:16:43 +0200

    Partial migration to kts
    app module build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,13 @@ plugins {
     id ("org.jetbrains.kotlin.android")
 }
 
+val composeVersion = "1.1.1"
+
 android {
     compileSdk = 31
 
     defaultConfig {
-        applicationId ("pl.wojtach.silvarerum2")
+        applicationId = "pl.wojtach.silvarerum2"
         minSdk = 28
         targetSdk = 31
         versionCode = 1
@@ -20,8 +22,8 @@ android {
     }
 
     buildTypes {
-        release {
-            minifyEnabled = false
+        getByName("release"){
+            isMinifyEnabled = false
             proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
@@ -36,7 +38,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = compose_version
+        kotlinCompilerExtensionVersion = composeVersion
     }
     packagingOptions {
         resources {
@@ -48,18 +50,18 @@ android {
 dependencies {
 
     implementation ("androidx.core:core-ktx:1.7.0")
-    implementation ("androidx.compose.ui:ui:$compose_version")
-    implementation ("androidx.compose.material:material:$compose_version")
-    implementation ("androidx.compose.ui:ui-tooling-preview:$compose_version")
+    implementation ("androidx.compose.ui:ui:$composeVersion")
+    implementation ("androidx.compose.material:material:$composeVersion")
+    implementation ("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
     implementation ("androidx.activity:activity-compose:1.4.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
-    implementation ("androidx.compose.material:material-icons-extended:$compose_version")
+    implementation ("androidx.compose.material:material-icons-extended:$composeVersion")
 
-    def nav_version = "2.4.2"
+    val nav_version = "2.4.2"
     implementation ("androidx.navigation:navigation-compose:$nav_version")
 
-    def room_version = "2.4.2"
+    val room_version = "2.4.2"
     implementation ("androidx.room:room-runtime:$room_version")
     annotationProcessor ("androidx.room:room-compiler:$room_version")
 //    ksp ("androidx.room:room-compiler:$roomVersion")
@@ -68,6 +70,6 @@ dependencies {
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.3")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$compose_version")
-    debugImplementation ("androidx.compose.ui:ui-tooling:$compose_version")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    debugImplementation ("androidx.compose.ui:ui-tooling:$composeVersion")
 }
