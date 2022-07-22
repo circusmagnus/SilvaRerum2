d0515c764f20045a4cc94f43dbe99d9027d78646
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2022-07-22 22:28:05 +0200

    Comment out draggable cell

--- app/src/main/java/pl/wojtach/silvarerum2/NoteListScreen.kt
+++ app/src/main/java/pl/wojtach/silvarerum2/NoteListScreen.kt
@@ -1,21 +1,13 @@
 package pl.wojtach.silvarerum2
 
 import android.util.Log
-import androidx.compose.foundation.gestures.DraggableState
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.layout.offset
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.unit.IntOffset
 import pl.wojtach.silvarerum2.manualdi.notesDeps
 import pl.wojtach.silvarerum2.utils.collectWhileStarted
 import pl.wojtach.silvarerum2.widgets.AddNoteButton
@@ -24,7 +16,6 @@ import pl.wojtach.silvarerum2.widgets.EditNoteButton
 import pl.wojtach.silvarerum2.widgets.NoteList
 import pl.wojtach.silvarerum2.widgets.ShortNote
 import pl.wojtach.silvarerum2.widgets.SilvaRerumHeader
-import kotlin.math.roundToInt
 
 @Composable
 fun NoteListScreen(onNoteClick: (NoteSnapshot) -> Unit, onNoteAdd: (NoteSnapshot) -> Unit, onNoteEdit: (NoteSnapshot) -> Unit) {
@@ -53,17 +44,17 @@ fun NoteListScreen(onNoteClick: (NoteSnapshot) -> Unit, onNoteAdd: (NoteSnapshot
 
 @Composable
 fun ShortNoteCellFactory(note: NoteSnapshot, model: NoteListModel, onNoteClick: (NoteSnapshot) -> Unit, onNoteEdit: (NoteSnapshot) -> Unit) {
-    var yOffset by remember { mutableStateOf(0f) }
+//    var yOffset by remember { mutableStateOf(0f) }
     ShortNote(
         note = note,
-        modifier = Modifier
-            .offset { IntOffset(0, yOffset.roundToInt()) }
-            .draggable(
-            orientation = Orientation.Vertical,
-            state = DraggableState { delta ->
-                yOffset +=delta
-            }
-        ),
+//        modifier = Modifier
+//            .offset { IntOffset(0, yOffset.roundToInt()) }
+//            .draggable(
+//            orientation = Orientation.Vertical,
+//            state = DraggableState { delta ->
+//                yOffset +=delta
+//            }
+//        ),
         onClick = { onNoteClick(note) },
         DeleteButton = { DeleteNoteButton { model.delete(note) } },
         EditButton = { EditNoteButton { onNoteEdit(note) } },)
