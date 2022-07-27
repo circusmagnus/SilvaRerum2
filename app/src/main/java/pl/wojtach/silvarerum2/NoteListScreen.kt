c2c728bf58afaa098fcd49f29faa653432b76bdf
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2022-07-28 01:03:40 +0200

    Reorderable list
    Still some strange reorder choices

--- app/src/main/java/pl/wojtach/silvarerum2/NoteListScreen.kt
+++ app/src/main/java/pl/wojtach/silvarerum2/NoteListScreen.kt
@@ -1,20 +1,14 @@
 package pl.wojtach.silvarerum2
 
 import android.util.Log
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.zIndex
 import pl.wojtach.silvarerum2.manualdi.notesDeps
 import pl.wojtach.silvarerum2.utils.collectWhileStarted
 import pl.wojtach.silvarerum2.widgets.AddButton
@@ -23,7 +17,6 @@ import pl.wojtach.silvarerum2.widgets.EditNoteButton
 import pl.wojtach.silvarerum2.widgets.ReorderableList
 import pl.wojtach.silvarerum2.widgets.ShortNote
 import pl.wojtach.silvarerum2.widgets.SilvaRerumHeader
-import kotlin.math.roundToInt
 
 @Composable
 fun NoteListScreen(onNoteClick: (NoteSnapshot) -> Unit, onNoteAdd: (NoteSnapshot) -> Unit, onNoteEdit: (NoteSnapshot) -> Unit) {
@@ -31,36 +24,10 @@ fun NoteListScreen(onNoteClick: (NoteSnapshot) -> Unit, onNoteAdd: (NoteSnapshot
     val scope = rememberCoroutineScope()
     val model = remember(key1 = scope) { notesDeps().noteListModel(scope) }
     val currentList by model.state.collectWhileStarted(lifecycleOwner = lifecycleOwner)
-    val lazyListState = rememberLazyListState()
-    val draggedNote = remember(key1 = currentList) {
-        mutableStateOf<NoteSnapshot?>(null)
-    }
-    val draggedNoteIndex = remember(key1 = currentList) {
-        mutableStateOf(0)
-    }
-    val listCellFactory: @Composable (Modifier, NoteSnapshot) -> Unit = { modifier, note ->
-        val draggedAmountY = remember { mutableStateOf(0f) }
 
+    val listCellFactory: @Composable (Modifier, NoteSnapshot) -> Unit = { modifier, note ->
         ShortNote(
-            modifier = modifier
-                .graphicsLayer {
-                    translationY = if (note == draggedNote.value) draggedAmountY.value else 0f
-                }.zIndex(if (note == draggedNote.value) 1f else 0f)
-                .pointerInput(Unit) {
-                    detectDragGesturesAfterLongPress(
-                        onDragStart = { offset -> draggedNote.value = note; draggedNoteIndex.value = currentList.indexOf(note) },
-                        onDrag = { change, offset -> draggedAmountY.value += offset.y },
-                        onDragEnd = {
-                            val visibleItems = lazyListState.layoutInfo.visibleItemsInfo
-                            Log.d("lw", "visibleItems: $visibleItems, draggedNoteIndex: $draggedNoteIndex")
-                            val dragged = visibleItems.first { it.index == draggedNoteIndex.value }
-                            val hoveredOver = visibleItems.firstOrNull { item -> dragged.offset + draggedAmountY.value.roundToInt() in item.offset..item.offset + item.size }
-                            draggedNote.value = null
-                            draggedAmountY.value = 0f
-                            hoveredOver?.let { model.reorder(draggedNoteIndex.value, it.index) }
-                        }
-                    )
-                },
+            modifier = modifier,
             note = note,
             onClick = { onNoteClick(note) },
             DeleteButton = { DeleteNoteButton { model.delete(note) } },
@@ -68,15 +35,15 @@ fun NoteListScreen(onNoteClick: (NoteSnapshot) -> Unit, onNoteAdd: (NoteSnapshot
         )
     }
 
-    Log.d("lw", "NoteListScreen composed: $currentList")
+    Log.d("lw", "NoteListScreen composed")
 
     NoteListLayout(
         topBar = { TopAppBar { SilvaRerumHeader() } },
         noteListUi = {
             ReorderableList(
-                lazyListState = lazyListState,
                 reorderableItems = currentList,
-                ListCell = listCellFactory
+                ListCell = listCellFactory,
+                onReorder = { fromIndex, toIndex -> model.reorder(fromIndex, toIndex) }
             )
         },
         floatingButton = {
--- app/src/main/java/pl/wojtach/silvarerum2/widgets/UiElements.kt
+++ app/src/main/java/pl/wojtach/silvarerum2/widgets/UiElements.kt
@@ -2,11 +2,13 @@ package pl.wojtach.silvarerum2.widgets
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyListItemInfo
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
@@ -18,15 +20,23 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Undo
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import pl.wojtach.silvarerum2.NoteId
 import pl.wojtach.silvarerum2.NoteSnapshot
 import pl.wojtach.silvarerum2.Timestamp
 import pl.wojtach.silvarerum2.utils.HasStableId
+import kotlin.math.roundToInt
 
 @Composable
 @Preview
@@ -62,18 +72,51 @@ fun UndoButton(onClick: () -> Unit = {}) {
 
 @Composable
 fun <T : HasStableId> ReorderableList(
-    lazyListState: LazyListState = LazyListState(),
     reorderableItems: List<T>,
-    ListCell: @Composable (Modifier, T) -> Unit
+    ListCell: @Composable (Modifier, T) -> Unit,
+    onReorder: (fromIndex: Int, toIndex: Int) -> Unit
 ) {
+    val lazyListState = rememberLazyListState()
+    var draggedElement by remember(key1 = reorderableItems) {
+        mutableStateOf<LazyListItemInfo?>(null)
+    }
+    var draggedElementIndex by remember(key1 = reorderableItems) {
+        mutableStateOf<Int>(-1)
+    }
+    var draggedDistanceY by remember(key1 = reorderableItems) {
+        mutableStateOf(0f)
+    }
 
     LazyColumn(
+        modifier = Modifier.pointerInput(key1 = reorderableItems) {
+            detectDragGesturesAfterLongPress(
+                onDragStart = { offset ->
+                    lazyListState.layoutInfo.visibleItemsInfo
+                        .first { listItem -> offset.y.roundToInt() in listItem.offset..listItem.offset + listItem.size }
+                        .also { found -> draggedElement = found; draggedElementIndex = found.index }
+                },
+                onDrag = { _, dragAmount -> draggedDistanceY += dragAmount.y },
+                onDragEnd = {
+                    val visibleItems = lazyListState.layoutInfo.visibleItemsInfo
+                    val hoveredOver = visibleItems.firstOrNull { draggedElement!!.offset + draggedElement!!.size + draggedDistanceY.roundToInt() in it.offset..it.offset + it.size }
+                    hoveredOver?.let { onReorder(draggedElementIndex, it.index) }
+                    draggedDistanceY = 0f
+                }
+            )
+        },
         state = lazyListState
     ) {
-        items(
+        itemsIndexed(
             reorderableItems,
-            { item -> item.id },
-            { item -> ListCell(Modifier, item) }
+            { _, item -> item.id },
+            { index, item ->
+                ListCell(
+                    Modifier
+                        .graphicsLayer { translationY = if (index == draggedElementIndex) draggedDistanceY else 0f }
+                        .zIndex(if (index == draggedElementIndex) 1f else 0f),
+                    item
+                )
+            }
         )
     }
 }
