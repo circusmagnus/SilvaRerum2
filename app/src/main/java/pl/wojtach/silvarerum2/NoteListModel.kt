5cf7a3208cd5baba5b793f3383832f44f0f328d1
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2022-08-02 00:34:48 +0200

    Clearance

--- app/src/main/java/pl/wojtach/silvarerum2/NoteListModel.kt
+++ app/src/main/java/pl/wojtach/silvarerum2/NoteListModel.kt
@@ -1,6 +1,5 @@
 package pl.wojtach.silvarerum2
 
-import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -15,12 +14,18 @@ class NoteListModel(scope: CoroutineScope, private val notesDao: NotesDao) : Cor
 
     val state: StateFlow<List<NoteSnapshot>> = notesDao
         .getAll()
-        .map { entities -> entities.map { it.asNote() } }
-        .map { notes -> notes.sortedWith(NoteListComparator()) }
+        .map { entities -> entities.map { it.asNote() }.sortedWith(NoteListComparator()) }
         .stateIn(scope, SharingStarted.WhileSubscribed(), emptyList())
 
     fun delete(note: NoteSnapshot) {
-        launch { notesDao.delete(note.toRoomEntity()) }
+        launch {
+            val items = state.value
+            notesDao.delete(note.toRoomEntity())
+            val indexReductionRange = items.subList(0, items.indexOf(note))
+            indexReductionRange
+                .map { note -> note.copy(reversedShowIndex = note.reversedShowIndex - 1).toRoomEntity() }
+                .let { notesDao.updateAll(it) }
+        }
     }
 
     fun addNew(): NoteSnapshot {
@@ -35,45 +40,26 @@ class NoteListModel(scope: CoroutineScope, private val notesDao: NotesDao) : Cor
     }
 
     fun reorder(fromPosition: Int, toPosition: Int) {
-        Log.d("lw", "dragged note from position: $fromPosition reordered to position: $toPosition")
-        val items = state.value
-        val from = items[fromPosition]
-        val to = items[toPosition]
-        val reorderedItem = from.copy(reversedShowIndex = to.reversedShowIndex)
-        val switchedAwayItem = to.copy(reversedShowIndex = from.reversedShowIndex)
-        Log.d("lw", "reordered $from to $reorderedItem, swithing with $to becoming $switchedAwayItem")
-
         launch {
+            val items = state.value
+            val from = items[fromPosition]
+            val to = items[toPosition]
+            val reorderedItem = from.copy(reversedShowIndex = to.reversedShowIndex)
+            val switchedAwayItem = to.copy(reversedShowIndex = from.reversedShowIndex)
             listOf(reorderedItem.toRoomEntity(), switchedAwayItem.toRoomEntity())
                 .let { notesDao.updateAll(it) }
         }
-//        val isReorderUp = fromPosition > toPosition
-//        val switchedItems = if (isReorderUp) {
-//            items.subList(toPosition, fromPosition)
-//                .map { toBeSwitchedDown -> toBeSwitchedDown.copy(reversedShowIndex = toBeSwitchedDown.reversedShowIndex - 1) }
-//        } else {
-//            items.subList(fromPosition + 1, toPosition + 1)
-//                .map { toBeSwitchedUp -> toBeSwitchedUp.copy(reversedShowIndex = toBeSwitchedUp.reversedShowIndex + 1) }
-//        }
-//        launch {
-//            (switchedItems + reorderedItem)
-//                .map { updatedNote -> updatedNote.toRoomEntity() }
-//                .let { updatedNotes -> notesDao.updateAll(updatedNotes) }
-//        }
     }
 
     private class NoteListComparator : Comparator<NoteSnapshot> {
 
         override fun compare(first: NoteSnapshot, second: NoteSnapshot): Int {
             val byIndex = (first.reversedShowIndex compareTo second.reversedShowIndex) * -1
+            if (byIndex != 0) return byIndex
+
             val byLastModification = (first.lastModified.value compareTo second.lastModified.value) * -1
-            return when {
-                byIndex != 0            -> byIndex
-                byLastModification != 0 -> byLastModification
-                else                    -> {
-                    (first.created.value compareTo second.created.value) * -1
-                }
-            }
+            return if (byLastModification != 0) byLastModification
+            else (first.created.value compareTo second.created.value) * -1
         }
     }
 }
--- app/src/main/java/pl/wojtach/silvarerum2/widgets/ReorderableList.kt
+++ app/src/main/java/pl/wojtach/silvarerum2/widgets/ReorderableList.kt
@@ -25,41 +25,14 @@ fun <T : HasStableId> ReorderableList(
     ListCell: @Composable (Modifier, T) -> Unit,
     onReorder: (fromIndex: Int, toIndex: Int) -> Unit
 ) {
-//    val lazyListState = rememberLazyListState()
-//    var draggedElement by remember(key1 = reorderableItems) {
-//        mutableStateOf<LazyListItemInfo?>(null)
-//    }
-//    var draggedElementIndex by remember(key1 = reorderableItems) {
-//        mutableStateOf<Int>(-1)
-//    }
-//    var draggedDistanceY by remember(key1 = reorderableItems) {
-//        mutableStateOf(0f)
-//    }
-
     val state = rememberReorderableListState(onReorder = onReorder)
 
     LazyColumn(
         modifier = Modifier.pointerInput(key1 = Unit) {
             detectDragGesturesAfterLongPress(
-                onDragStart = { offset ->
-                    state.onDragStart(offset)
-//                    lazyListState.layoutInfo.visibleItemsInfo
-//                        .first { listItem -> offset.y.roundToInt() in listItem.offset..listItem.offset + listItem.size }
-//                        .also { found -> draggedElement = found; draggedElementIndex = found.index }
-                },
-                onDrag = { _, dragAmount ->
-                    state.onDrag(dragAmount)
-//                    draggedDistanceY += dragAmount.y
-                         },
-                onDragEnd = {
-                    state.onDragInterrupt()
-//                    val visibleItems = lazyListState.layoutInfo.visibleItemsInfo
-//                    Log.d("lw", "onDragEnd, dragged dist: $draggedDistanceY, dragged element: $draggedElement")
-//                    val toIndex = visibleItems.findDraggedToIndexOrNull(draggedElement!!, draggedDistanceY)
-//                    toIndex?.let { onReorder(draggedElementIndex, it) }
-//
-//                    draggedDistanceY = 0f
-                }
+                onDragStart = { offset -> state.onDragStart(offset) },
+                onDrag = { _, dragAmount -> state.onDrag(dragAmount) },
+                onDragEnd = { state.onDragInterrupt() }
             )
         },
         state = state.lazyListState
@@ -70,7 +43,9 @@ fun <T : HasStableId> ReorderableList(
             { index, item ->
                 ListCell(
                     Modifier
-                        .graphicsLayer { translationY = if (index == state.currentDraggedElementIndex) state.calculatedDisplacement else 0f }
+                        .graphicsLayer {
+                            translationY = if (index == state.currentDraggedElementIndex) state.calculatedDisplacement else 0f
+                        }
                         .zIndex(if (index == state.currentDraggedElementIndex) 1f else 0f),
                     item
                 )
@@ -108,6 +83,7 @@ private class ReorderableListState(
 ) {
     private var initiallyDraggedElement by mutableStateOf<LazyListItemInfo?>(null)
     var currentDraggedElementIndex by mutableStateOf<Int>(-1)
+        private set
     private var draggedDistanceY by mutableStateOf(0f)
     private val currentlyDraggedElement: LazyListItemInfo?
         get() = lazyListState.getVisibleItemInfoFor(currentDraggedElementIndex)
@@ -127,20 +103,17 @@ private class ReorderableListState(
         draggedDistanceY += dragAmount.y
 
         val visibleItems = lazyListState.layoutInfo.visibleItemsInfo
-        val toIndex2 = visibleItems.findDraggedToIndexOrNull(currentlyDraggedElement!!, calculatedDisplacement)
+        val toIndex = visibleItems.findDraggedToIndexOrNull(currentlyDraggedElement!!, calculatedDisplacement)
 
-//        Log.d("lw", "onDragEnd, dragged dist: $draggedDistanceY, dragged element: $draggedElement")
-//        val toIndex = visibleItems.findDraggedToIndexOrNull(draggedElement!!, draggedDistanceY)
-
-        toIndex2
+        toIndex
             ?.takeIf { newIndex -> newIndex != currentDraggedElementIndex }
             ?.let { newIndex ->
-            onReorder(currentDraggedElementIndex, newIndex)
-            currentDraggedElementIndex = newIndex
-        }
+                onReorder(currentDraggedElementIndex, newIndex)
+                currentDraggedElementIndex = newIndex
+            }
     }
 
-    fun onDragInterrupt(){
+    fun onDragInterrupt() {
         draggedDistanceY = 0f
         initiallyDraggedElement = null
         currentDraggedElementIndex = -1
